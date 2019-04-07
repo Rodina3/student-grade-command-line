6490006fa6db4208896ae8b832f46887c57e1c61
Rodina3 <buaazhourong@163.com>
2019-04-07 22:34:57 +0800

    [Rong] Add model: Score, Student and Report

---
+++ src/main/java/com/tw/model/Student.java
@@ -0,0 +1,52 @@
+package com.tw.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Student {
+    private String studentNumber;
+    private String name;
+    private List<Score> scores =  new ArrayList<>();
+    private float totalScore;
+    private float averageScore;
+
+    public String getStudentNumber() {
+        return studentNumber;
+    }
+
+    public void setStudentNumber(String studentNumber) {
+        this.studentNumber = studentNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Score> getScores() {
+        return scores;
+    }
+
+    public void setScores(List<Score> scores) {
+        this.scores = scores;
+    }
+
+    public float getTotalScore() {
+        return totalScore;
+    }
+
+    public void setTotalScore(float totalScore) {
+        this.totalScore = totalScore;
+    }
+
+    public float getAverageScore() {
+        return averageScore;
+    }
+
+    public void setAverageScore(float averageScore) {
+        this.averageScore = averageScore;
+    }
+}
