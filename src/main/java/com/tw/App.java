1b2d655bfa5e704838a9b45d2532f83d75b2bdbf
Rodina3 <buaazhourong@163.com>
2019-04-14 00:15:09 +0800

    [Rong] Implement Router register and init router in App

--- src/main/java/com/tw/App.java
+++ src/main/java/com/tw/App.java
@@ -2,6 +2,10 @@ package com.tw;
 
 import com.tw.controller.Router;
 import com.tw.controller.Status;
+import com.tw.controller.command.AddStudentCmd;
+import com.tw.controller.command.HomeCmd;
+import com.tw.controller.command.PrintReportCmd;
+import com.tw.controller.command.SelectMenuCmd;
 import com.tw.controller.dto.Request;
 import com.tw.controller.dto.Response;
 
@@ -11,17 +15,32 @@ import static com.tw.controller.Status.EXIT;
 import static com.tw.controller.Status.HOME;
 
 public class App {
+    private static Router router;
+    private static Status currentStatus;
+    private static boolean isInputRequired;
+    private static Scanner scanner;
 
     public static void main(String[] args) {
-        Router router = new Router();
-        Scanner sc = new Scanner(System.in);
+        init();
+        process();
+    }
 
-        Status currentStatus = HOME;
-        boolean isInputRequired = false;
+    private static void init() {
+        router = new Router();
+        router.register(Status.HOME, new HomeCmd());
+        router.register(Status.MENU, new SelectMenuCmd());
+        router.register(Status.ADD_STUDENT, new AddStudentCmd());
+        router.register(Status.PRINT_REPORT, new PrintReportCmd());
 
+        currentStatus = HOME;
+        isInputRequired = false;
+        scanner = new Scanner(System.in);
+    }
+
+    private static void process() {
         while (currentStatus != EXIT) {
-            Request request = buildRequest(sc, currentStatus, isInputRequired);
-            Response response = getResponse(router, request);
+            Request request = buildRequest();
+            Response response = getResponse(request);
 
             System.out.println(response.getPage());
             currentStatus = Status.valueOf(response.getStatus());
@@ -29,15 +48,14 @@ public class App {
         }
     }
 
-    private static Response getResponse(Router router, Request request) {
+    private static Response getResponse(Request request) {
         return router.dispatch(request);
     }
 
-    private static Request buildRequest(Scanner sc, Status currentStatus, boolean isInputRequired) {
+    private static Request buildRequest() {
         Request request = new Request();
         if (isInputRequired) {
-            String input = sc.nextLine();
-            request.setInput(input);
+            request.setInput(scanner.nextLine());
         }
         request.setCurrentStatus(currentStatus.toString());
         return request;
--- src/main/java/com/tw/controller/Router.java
+++ src/main/java/com/tw/controller/Router.java
@@ -1,10 +1,6 @@
 package com.tw.controller;
 
-import com.tw.controller.command.AddStudentCmd;
 import com.tw.controller.command.Command;
-import com.tw.controller.command.HomeCmd;
-import com.tw.controller.command.PrintReportCmd;
-import com.tw.controller.command.SelectMenuCmd;
 import com.tw.controller.dto.Request;
 import com.tw.controller.dto.Response;
 
@@ -14,16 +10,17 @@ import java.util.Map;
 public class Router {
     private Map<Status, Command> router = new HashMap<>();
 
-    public Router() {
-        router.put(Status.HOME, new HomeCmd());
-        router.put(Status.MENU, new SelectMenuCmd());
-        router.put(Status.ADD_STUDENT, new AddStudentCmd());
-        router.put(Status.PRINT_REPORT, new PrintReportCmd());
-    }
-
     public Response dispatch(Request request) {
         Command command = router.get(Status.valueOf(request.getCurrentStatus()));
         return command.exec(request);
+    }
+
+    public void register(Status status, Command command) {
+        router.put(status, command);
+    }
+
+    public void remove(Status status) {
+        router.remove(status);
     }
 
 }
